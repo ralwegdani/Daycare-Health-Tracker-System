5e80a66a5297f89a47dab4346f45f9a0a984f11d
Reham Alwegdani <ralwegdanii@gmail.com>
2026-02-18 22:43:11 +0300

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author la690
- */
 public class User {
     // common fields for both child and staff
     private int id;                 
