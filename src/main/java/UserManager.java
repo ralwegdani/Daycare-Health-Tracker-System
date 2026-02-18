2e200787375daee137be710719525396ec9aab7b
Reham Alwegdani <ralwegdanii@gmail.com>
2026-02-18 22:43:32 +0300

    Update UserManager.java

--- src/main/java/UserManager.java
+++ src/main/java/UserManager.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-/**
- *
- * @author la690
- */
+
 public class UserManager {
     
     //ArryList to handel all user objects
