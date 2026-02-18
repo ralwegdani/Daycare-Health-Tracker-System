b1b3641d2c3dffe1bcbf60c552679d25c1964c89
Reham Alwegdani <ralwegdanii@gmail.com>
2026-02-18 22:44:05 +0300

    Update EnterChildHealthInfoTest.java

--- src/test/java/EnterChildHealthInfoTest.java
+++ src/test/java/EnterChildHealthInfoTest.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 import org.junit.Test;
 import static org.junit.Assert.*;
-/**
- *
- * @author Nadaa
- */
 public class EnterChildHealthInfoTest {
     @Test
     public void testAddHealthInfoSuccess() {
